131cd73c24d3f6e0d0bfc9a19f01d507531ab163
Dimitri Huisman <diman@huisman.xyz>
2021-11-16 21:45:23 +0000

    Added required  spaces

--- tests/deploy.sh
+++ tests/deploy.sh
@@ -15,7 +15,7 @@ fi
 #Images are built with tag PINNED_MAILU_VERSION (x.y.z).
 #We are tagging them as well with MAILU_VERSION (x.y)
 #After that, both tags are pushed to the docker repository.
-if [ "$PINNED_MAILU_VERSION" != ""] && ["$BRANCH" != "master" ]
+if [ "$PINNED_MAILU_VERSION" != "" ] && [ "$BRANCH" != "master" ]
 then
   images=$(docker-compose -f build.yml config | grep 'image: ' | awk -F ':' '{ print $2 }')
   for image in $images
@@ -31,7 +31,7 @@ then
 fi
 
 #Deploy for master. For master we only publish images with tag master
-if [ "$PINNED_MAILU_VERSION" != ""] && ["$BRANCH" == "master" ]
+if [ "$PINNED_MAILU_VERSION" != "" ] && [ "$BRANCH" == "master" ]
 then
 #Images are built with PINNED_MAILU_VERSION.
 #We are tagging them as well with MAILU_VERSION
